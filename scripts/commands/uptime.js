b7d1f94fb559fee5d69a9df8c937c0b058b916b3
sormishourov333 <shourovssm@gmail.com>
2024-08-25 17:47:56 +0600

    Update uptime.js

--- scripts/commands/uptime.js
+++ scripts/commands/uptime.js
@@ -129,9 +129,9 @@ registerFont(__dirname + `/cache/CaviarDreams.ttf`, {
     });
     ctx.textAlign = "start";
     ctx.font = "55px time";
-    ctx.fillText("FARHAN_ISLAM", 930, 540)
-    ctx.fillText("FARHAN_ISLAM", 930, 610)
-    ctx.fillText("FARHAN_ISLAM", 930, 690)
+    ctx.fillText("KING_SHOUROV", 930, 540)
+    ctx.fillText("KING_SHOUROV", 930, 610)
+    ctx.fillText("KING_SHOUROV", 930, 690)
     ctx.restore();
     ctx.save();
     ctx.beginPath();
